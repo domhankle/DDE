bb5c5d90d0127bb67246dbd491266501b850825d
Dom Hankle <domhankle03@gmail.com>
2024-06-02 01:07:21 -0400

    Fixing indents and removing comment

--- src/dde.cpp
+++ src/dde.cpp
@@ -1,8 +1,7 @@
 #include <DDE/Engine/RenderEngine.hpp>
 
-//Adding comment
 int main() {
-    RenderEngine engine;
-    engine.start();
-    return 0;
+  RenderEngine engine;
+  engine.start();
+  return 0;
 }
