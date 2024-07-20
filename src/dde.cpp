66e573b18b0bac1d5405a5f1ec6c75b2b1e6a6c2
domhankle <domhankle03@gmail.com>
2024-07-20 10:20:44 -0400

    Drawing triangle and square in dde.cpp

--- src/dde.cpp
+++ src/dde.cpp
@@ -10,8 +10,10 @@
 #include <DDE/Shader/Shader.hpp>
 #include <DDE/Shader/ShaderProgram.hpp>
 
-void drawFunction(DDE::Pencil &pencil, DDE::Quad &square) {
+void drawFunction(DDE::Pencil &pencil, DDE::Quad &square,
+                  DDE::Triangle &triange) {
   pencil.draw(square);
+  pencil.draw(triange);
 }
 
 int main() {
@@ -19,12 +21,15 @@ int main() {
   DDE::RenderEngine engine;
 
   DDE::Vertex v1{0.0f, 0.5f}, v3{0.5f, 0.0f}, v4{-0.5f, 0.0f}, v2{0.0f, -0.5f};
-
   DDE::Quad square{v1, v2, v3, v4};
+
+  DDE::Vertex v5{-1.0f, 1.0f}, v6{0.0f, 1.0f}, v7{-0.5, 0.5};
+  DDE::Triangle triangle{v5, v6, v7};
   DDE::Pencil pencil;
 
   // Start the Rendering Enginge
-  engine.start(drawFunction, std::ref(pencil), std::ref(square));
+  engine.start(drawFunction, std::ref(pencil), std::ref(square),
+               std::ref(triangle));
 
   return 0;
 }
